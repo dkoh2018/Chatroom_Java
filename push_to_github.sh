bc0eb6f6c1a84bad46c18e944738a555f70fdbad
dkoh2018 <105094753+dkoh2018@users.noreply.github.com>
2024-10-01 23:19:50 -0500

    fixed build

--- push_to_github.sh
+++ push_to_github.sh
@@ -10,7 +10,6 @@ git fetch origin
 git rebase origin/main
 
 if [ $? -ne 0 ]; then
-    echo "Rebase failed. Attempting to resolve conflicts..."
     git rebase --continue || git rebase --abort
 fi
 
--- src/main/java/com/chatapp/App.java
+++ src/main/java/com/chatapp/App.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.chatapp;
 
 import java.util.Scanner;
 
